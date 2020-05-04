fb026c9e047f9884f28e3f08ac12542b9539d5ad
Aidy <aidyszh@gmail.com>
2020-05-04 11:47:34 +0800

    init commit

---
+++ UIColorExtension.swift
@@ -0,0 +1,20 @@
+//
+//  UIColorExtension.swift
+//  KidCalculator
+//
+//  Created by Aidy Sun on 2020/3/12.
+//  Copyright © 2020 Aidy. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+extension UIColor {
+    private class func rgbValue(_ f: CGFloat) -> CGFloat {
+        return f / 255
+    }
+    
+    class func initWithRGB255(red r: CGFloat, green g: CGFloat, blue b: CGFloat, alpha a:  CGFloat = 1.0) -> UIColor {
+        return UIColor(red: rgbValue(r), green: rgbValue(g), blue: rgbValue(b), alpha: a)
+    }
+}
