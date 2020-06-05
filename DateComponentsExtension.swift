4476ecb3b5b598c9abb80881bd36906c19b7cb9b
Aidy <aidyszh@gmail.com>
2020-06-05 22:53:44 +0800

    DateComponentsExtension.swift

---
+++ DateComponentsExtension.swift
@@ -0,0 +1,33 @@
+//
+//  DateComponentsExtension.swift
+//  ArtClock
+//
+//  Created by Aidy Sun on 2020/6/5.
+//  Copyright © 2020 Aidy Sun. All rights reserved.
+//
+
+import Foundation
+
+extension DateComponents {
+    
+    func isHalfHour() -> Bool {
+        return isSecond(0) && (isMinute(0) || isMinute(30))
+    }
+    
+    func isWholeHour() -> Bool {
+        return isMinute(0) && isSecond(0)
+    }
+    
+    func isWholeHourTheTimesOf(_ i: Int) -> Bool {
+        return isWholeHour() && (nil != hour) && (i != 0) && (hour! % i == 0)
+    }
+    
+    private func isMinute(_ m: Int) -> Bool {
+        return (self.minute == m)
+    }
+    
+    private func isSecond(_ m: Int) -> Bool {
+        return (self.second == m)
+    }
+    
+}
