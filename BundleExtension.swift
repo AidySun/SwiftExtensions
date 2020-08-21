9fe902240466e63341dceb95a3b637ebbeb25ce8
Aidy <aidyszh@gmail.com>
2020-08-21 11:00:17 +0800

    get app name in Bundle extension

--- BundleExtension.swift
+++ BundleExtension.swift
@@ -3,6 +3,10 @@ import Foundation
 extension Bundle {
 
     func getAppVersion() -> String {
-        return (Bundle.main.infoDictionary?["CFBundleVersion"] as? String) ?? ""
+        return (Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String) ?? ""
+    }
+    
+    func getAppName() -> String {
+        return (Bundle.main.infoDictionary?["CFBundleName"] as? String) ?? ""
     }
 }
