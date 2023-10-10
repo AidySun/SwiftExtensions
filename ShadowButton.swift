62d867cceb5c8c9a167bbd6e99e5bb7665b05f40
Aidy <aidyszh@gmail.com>
2023-10-10 15:41:15 +0800

    add ShadowButton

---
+++ ShadowButton.swift
@@ -0,0 +1,39 @@
+//
+//  ShadowButton.swift
+//  SwiftExtensions  
+//
+//  Created by Aidy Sun on 2023/10/10.
+//
+
+import UIKit
+
+class ShadowButton: UIButton {
+}
+
+@IBDesignable extension ShadowButton {
+
+    @IBInspectable var shadowRadius: CGFloat {
+        get { return layer.shadowRadius }
+        set { layer.shadowRadius = newValue }
+    }
+
+    @IBInspectable var shadowOpacity: CGFloat {
+        get { return CGFloat(layer.shadowOpacity) }
+        set { layer.shadowOpacity = Float(newValue) }
+    }
+
+    @IBInspectable var shadowOffset: CGSize {
+        get { return layer.shadowOffset }
+        set { layer.shadowOffset = newValue }
+    }
+
+    @IBInspectable var shadowColor: UIColor? {
+        get {
+            guard let cgColor = layer.shadowColor else {
+                return nil
+            }
+            return UIColor(cgColor: cgColor)
+        }
+        set { layer.shadowColor = newValue?.cgColor }
+    }
+}
