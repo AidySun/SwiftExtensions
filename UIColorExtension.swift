83b425f0387d3bf5aaaa6936e707b220cb7273d4
Aidy <aidyszh@gmail.com>
2025-03-25 19:22:56 +0800

    initWithRGB255 no parameter name

--- UIColorExtension.swift
+++ UIColorExtension.swift
@@ -15,11 +15,15 @@ extension UIColor {
     private class func rgbValue(_ f: CGFloat) -> CGFloat {
         return f / 255
     }
-    
+
+    class func initWithRGB255(_ r: CGFloat, _ g: CGFloat, _ b: CGFloat, _  a: CGFloat = 1.0) -> UIColor {
+        return UIColor(red: rgbValue(r), green: rgbValue(g), blue: rgbValue(b), alpha: a)
+    }
+
     class func initWithRGB255(red r: CGFloat, green g: CGFloat, blue b: CGFloat, alpha a: CGFloat = 1.0) -> UIColor {
         return UIColor(red: rgbValue(r), green: rgbValue(g), blue: rgbValue(b), alpha: a)
     }
-    
+
     class func initWithRGB(_ r: CGFloat, _ g: CGFloat, _ b: CGFloat, _ a: CGFloat = 1.0) -> UIColor {
         return UIColor.initWithRGB255(red: r, green: g, blue: b, alpha: a)
     }
